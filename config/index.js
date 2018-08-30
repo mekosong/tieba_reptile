d0cc05c43a78d6f2e361a813f44d003a9e458a51
artworld <artworld@aworld.cn>
2018-08-30 09:45:34 +0800

    fix time error2

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
-console.log(`the NODE_ENV is:${process.env.NODE_ENV}`);
+console.log(`the NODE_ENV is:${process.env.NODE_ENV|| 'local'}`);
 module.exports = require(`./${process.env.NODE_ENV || 'local'}`);
